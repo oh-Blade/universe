b83e86e10a845bedb34f99dab2e5a7294c96437f
naikuoh <naikuoh@hupofintech.com>
2019-11-17 20:00:50 +0800

    leet code

---
+++ src/IntReverse.java
@@ -0,0 +1,33 @@
+/**
+ * @Classname IntReverse   7
+ * @Description TODO
+ * @DATE 2019/10/27 19:45
+ * @Created by Administrator
+ */
+public class IntReverse {
+    public static int reverse(int x) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(x);
+        if (x > 0) {
+            Long aLong = Long.valueOf(builder.reverse().toString());
+            if (aLong > Integer.MAX_VALUE) {
+                return 0;
+            } else {
+                return Math.toIntExact(aLong);
+            }
+        } else if (x < 0) {
+            builder.reverse().substring(0, builder.length() - 1);
+            Long aLong = -Long.valueOf(builder.reverse().toString());
+            if (aLong < Integer.MIN_VALUE) {
+                return 0;
+            } else {
+                return Math.toIntExact(aLong);
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+    }
+}
---
+++ src/TreeNode.java
@@ -0,0 +1,15 @@
+/**
+ * @Classname TreeNode
+ * @Description TODO
+ * @DATE 2019/11/11 23:01
+ * @Created by Administrator
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
