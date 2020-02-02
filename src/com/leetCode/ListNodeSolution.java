bf71bac5344d5de61aa02bcddc93a92cd1b2df5d
naikuo <naikuoh@hupofintech.com>
2020-02-02 16:56:12 +0800

    addTwoNumbers

--- src/com/leetCode/ListNodeSolution.java
+++ src/com/leetCode/ListNodeSolution.java
@@ -1,5 +1,7 @@
 package com.leetCode;
 
+import java.util.List;
+
 /**
  * @Description 2
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
@@ -12,34 +14,33 @@ package com.leetCode;
  * <p>
  * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
  * 输出：7 -> 0 -> 8
- * 原因：342 + 465 = 807
+ * 原因：342 + 665 = 1007
  * @Author naikuo
  * @Date 2020/1/1 9:21 AM
  */
 public class ListNodeSolution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode node = new ListNode(0);
-        ListNode p = l1, q = l2, curr = node;
-        int currency = 0;
-        while (p != null || q != null) {
-            int m = p == null ? 0 : p.val;
-            int n = q == null ? 0 : q.val;
-            int sum = m + n + currency;
-            currency = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
 
-            if (p != null) {
-                p = p.next;
-            }
-            if (q != null) {
-                q = q.next;
-            }
+        ListNode outNode = new ListNode(0);
+
+        ListNode l1temp = l1;
+        ListNode l2temp = l2;
+        ListNode curry = outNode;
+        int commit = 0;
+        while (l1temp != null || l2temp != null) {
+            int value = (l1temp != null ? l1temp.val : 0) + (l2temp != null ? l2temp.val : 0) + commit;
+            commit = value / 10;
+            curry.next = new ListNode(value % 10);
+            curry = curry.next;
+
+            l1temp = l1temp != null ? l1temp.next : null;
+            l2temp = l2temp != null ? l2temp.next : null;
         }
-        if (currency == 1) {
-            curr.next = new ListNode(1);
+        if (commit == 1) {
+            curry.next = new ListNode(1);
         }
-        return node.next;
+        return outNode.next;
+
     }
 
 
