b69b590af84bdabfd80503c58ffdd3e10f07c74a
naikuo <naikuoh@hupofintech.com>
2020-02-05 00:19:04 +0800

    MinStack

--- src/com/leetCode/Test.java
+++ src/com/leetCode/Test.java
@@ -1,6 +1,6 @@
 package com.leetCode;
 
-import java.util.LinkedHashMap;
+import java.util.*;
 
 /**
  * @Description TODO
@@ -9,6 +9,74 @@ import java.util.LinkedHashMap;
  */
 public class Test {
 
+    @org.junit.Test
+    public void testDeque() {
+        /**
+         * 堆栈
+         */
+        Deque<String> stack = new ArrayDeque<>();
+        //入栈
+        stack.push("a");
+        stack.push("b");
+
+        //出栈
+        stack.pop();
+
+        //取栈顶
+        stack.peek();
+        stack.peekFirst();
+        //取栈底
+        stack.peekLast();
+
+        stack.contains("b");
+        stack.isEmpty();
+        stack.size();
+
+        /**
+         * 单项队列
+         */
+        Deque<String> queue = new ArrayDeque<>();
+        //进队列
+        queue.offer("a");
+        queue.offer("b");
+
+        //出队列
+        queue.poll();
+        //取队首和队尾
+        queue.peekFirst();
+        queue.peekLast();
+
+        queue.contains("b");
+        queue.isEmpty();
+        queue.size();
+
+        /**
+         * 双端队列
+         */
+        Deque<String> deque = new ArrayDeque<>();
+        //队首队尾插入
+        deque.offerFirst("a");
+        deque.offerLast("b");
+        //取队首队尾
+        deque.peekFirst();
+        deque.peekLast();
+
+        deque.contains("b");
+        deque.size();
+        deque.isEmpty();
+
+
+    }
+
+    @org.junit.Test
+    public void testMinStack() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(4);
+        stack.push(3);
+        stack.push(5);
+        stack.push(2);
+        System.out.println(stack);
+    }
 
     @org.junit.Test
     public void testMyCircularDeque() {
