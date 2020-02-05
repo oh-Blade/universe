cfa112353300a42c50922c20b1754055646641b8
naikuo <naikuoh@hupofintech.com>
2020-02-05 16:44:27 +0800

    BSTIterator

---
+++ src/com/leetCode/TreeNode.java
@@ -0,0 +1,17 @@
+package com.leetCode;
+
+/**
+ * @Description Definition for a binary tree node.
+ * @Author naikuo
+ * @Date 2020/2/5 4:02 PM
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
