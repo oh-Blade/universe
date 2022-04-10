ea7e2c8d16d6ff32f6527a77fc506913ed06f3f7
hounk801 <824275538@qq.com>
2022-04-10 17:24:49 +0800

    'decimal'

---
+++ src/test/java/com/huohou/universe/Temp.java
@@ -0,0 +1,12 @@
+package com.huohou.universe;
+
+import java.math.BigDecimal;
+
+/**
+ * @author huohou
+ */
+public class Temp {
+    public static void main(String[] args) {
+        System.out.println(new BigDecimal("123456789123456789123456789123456789123456789123456789.123456789123456789"));
+    }
+}
